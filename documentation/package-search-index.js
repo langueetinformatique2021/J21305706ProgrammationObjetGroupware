439b157f2329ccd75f0e113c4ca73924b27c252c
isha <isha@fedora>
2023-10-19 11:39:16 +0200

    projet

---
+++ documentation/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"ClaraProgrammationObjetetGroupware","l":"atelier02"}];updateSearchResults();
---
+++ src/atelier01/SimpleProgramme1.java
@@ -0,0 +1,12 @@
+package atelier01;
+
+public class SimpleProgramme1 {
+
+	public static void main(String[] args) {
+		System.out.println("Clara Picard") ;
+
+		
+		
+	}
+
+}
